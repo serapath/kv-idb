9fab37f0e2a84f78037b836a42bd3bb3692e3f17
serapath <dev@serapath.de>
2023-05-29 02:09:12 +0100

    release kv-idb version 2.0.0

--- README.md
+++ README.md
@@ -7,29 +7,27 @@ minimal zero-dependencies key-value store backed by indexedDB
 ```js
 var kvidb = require('kv-idb')
 
-var defaultName = 'store'
-
-var db = kvidb(/* defaultName */)
-
-var log = (e, x) => e ? console.error(e) : console.log(x)
-
-db.clear(log),                  // true
-db.put('foo', 'bar', log),      // true
-db.put('null', {}, log),        // true
-db.length(log),                 // 2
-db.keys(log),                   // ['foo', 'null']
-db.get('foo', log),             // 'bar'
-db.clear(log),                  // true
-db.get('foo', log),             // ERROR
-db.put('foo', 'bar1', log),     // true
-db.put('foo2', 'bar2', log),    // true
-db.put('foo3', 'bar3', log),    // true
-db.put('foo4', null, log),      // true
-db.put('foo5', undefined, log), // ERROR
-db.get('foo', log),             // 'bar1'
-db.get('foo3', log),            // 'bar3'
-db.del('foo3', log),            // true
-db.get('foo3', log),            // ERROR
-db.length(log),                 // 3
-db.keys(log),                   // ['foo', 'foo2', 'foo4']
+var db = kvidb()
+await db.clear(),                // true
+await db.put('foo', 'bar'),      // true
+await db.put('null', {}),        // true
+await db.length(),               // 2
+await db.keys(),                 // ["foo", "null"]
+await db.get('foo'),             // "bar"
+await db.clear(),                // true
+await db.get('foo'),             // undefined
+await db.put('foo', 'bar1'),     // true
+await db.put('foo2', 'bar2'),    // true
+await db.put('foo3', 'bar3'),    // true
+await db.get('foo3'),            // "bar3"
+await db.put('foo4', null),      // true
+await db.get('foo4'),            // null
+await db.put('foo5', undefined), // true
+await db.get('foo5'),            // undefined
+await db.get('foo'),             // "bar1"
+await db.get('foo3'),            // "bar3"
+await db.del('foo3'),            // true
+await db.get('foo3'),            // undefined
+await db.length(),               // 4
+await db.keys(),                 // ["foo", "foo2", "foo4", "foo5"]
 ```
--- bundle.js
+++ bundle.js
@@ -1,67 +1,87 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.kvidb = f()}})(function(){var define,module,exports;return (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.kvidb = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const indexedDB = window.indexedDB
-const console = window.console
 
 module.exports = kvidb
 
-const dbname = 'kvidb'
-// const dbopts = { keyPath: 'key' }
-const version = 1
-
-function kvidb (opts) {
-  const name = opts ? opts.name || ('' + opts) : 'store'
-  const scope = `${dbname}-${name}`
-  var IDB
-  const makeDB = done => {
-    var idb = indexedDB.open(dbname, version)
-    idb.onerror = e => console.error(`[${dbname}]`, idb.error)
-    idb.onupgradeneeded = () => idb.result.createObjectStore(scope/*, dbopts*/)
-    idb.onsuccess = () => done(IDB = idb.result)
-  }
-  const use = (mode, done) => {
-    const next = (IDB, tx) => (tx = IDB.transaction([scope], mode),
-      done(tx.objectStore(scope), tx))
-    IDB ? next(IDB) : makeDB(next)
-  }
-  const api = {
-    get: (key, done) => use('readonly', (store, tx) => {
-      const req = store.get('' + key)
-      tx.oncomplete = e => next(req.error, req.result)
-      const next = (e, x) => {
-        e ? done(e) : x === undefined ? done(`key "${key}" is undefined`)
-        : done(null, x)
-      }
-    }),
-    put: (key, val, done) => val === undefined ? done('`value` is undefined')
-      : use('readwrite', (store, tx) => {
-        const req = store.put(val, '' + key)
-        tx.oncomplete = e => done(req.error, !req.error)
-    }),
-    del: (key, done) => api.get('' + key, (e, x) => {
-      e ? done(e) : use('readwrite', (store, tx) => {
-        const req = store.delete('' + key)
-        tx.oncomplete = e => done(req.error, !req.error)
-      })
-    }),
-    clear: done => use('readwrite',  (store, tx) => {
-      const req = store.clear()
-      tx.oncomplete = e => done(req.error, !req.error)
-    }),
-    length: done => use('readwrite',  (store, tx) => {
-      const req = store.count()
-      tx.oncomplete = e => done(req.error, req.result)
-    }),
-    close: done => (IDB ? IDB.close() : makeDB(IDB => IDB.close()), done(null, true)),
-    batch: (ops, done) => done('@TODO: implement `.batch(...)`'),
-    keys: done => use('readonly', (store, tx, keys = []) => {
-      const openCursor = (store.openKeyCursor || store.openCursor)
-      const req = openCursor.call(store)
-      tx.oncomplete = e => done(req.error, req.error ? undefined : keys)
-      req.onsuccess = () => {
-        const x = req.result
-        if (x) (keys.push(x.key), x.continue())
-      }
+async function kvidb (db_name = 'db', store_name = 'store', version = 1) {
+  const idb = indexedDB.open(db_name, version)
+  idb.onupgradeneeded = () => idb.result.createObjectStore(store_name)
+  const db = await pfy(idb)
+  const kvstore = { get, put, del, batch, update, clear, close, keys, values, entries, length }
+  return kvstore
+  function pfy (request) {
+    return new Promise((resolve, reject) => {
+      request.oncomplete = request.onsuccess = () => resolve(request.result)
+      request.onabort = request.onerror = () => reject(request.error)
     })
+  }
+  function getstore () { return db.transaction(store_name, 'readonly').objectStore(store_name) }
+  function allstore () { return db.transaction(store_name, 'readwrite').objectStore(store_name) }
+  function close () { return db.close() }
+  function get (key) {
+    const store = getstore()
+    return pfy(store.get(key))
+  }
+  async function put (key, val) {
+    const store = allstore()
+    store.put(val, key)
+    return !await pfy(store.transaction)
+  }
+  async function del (key, val) {
+    const store = allstore()
+    store.delete(key)
+    return !await pfy(store.transaction)
+  }
+  async function clear () {
+    const store = allstore()
+    store.clear()
+    return !await pfy(store.transaction)
+  }
+  function batch () {
+    var ops = []
+    return { put, del, flush, destroy }
+    function put (key, val) { ops.push({ type: 'put', data: [key, val] }) }
+    function del (key, val) { ops.push({ type: 'del', data: [key, val] }) }
+    function destroy () { ops = undefined }
+    async function flush () {
+      for (var i = 0, len = ops.length; i < len; i++) {
+        const { type, data } = opts[i]
+        const store = allstore()
+        store[type](...data)
+      }
+      await pfy(allstore.transaction)
+      for (var i = 0; i < len; i++) kvstore?.[`on${ops[i].type}`](ops[i])
+      return true
+    }
+  }
+  function keys () {
+    const store = getstore()
+    return pfy(store.getAllKeys())
+  }
+  function values () {
+    const store = getstore()
+    return pfy(store.getAll())
+  }
+  function entries () {
+    const store = getstore()
+    return Promise.all([pfy(store.getAllKeys()), pfy(store.getAll())]).then(([ks, vs]) => ks.map((k, i) => [k, vs[i]]))
+  }
+  function length () {
+    const store = getstore()
+    return pfy(store.count())
+  }
+  async function update (key, updater) { return !await put(updater(await get(key))) }
+  /*
+  function createReadStream () {
+    // keys: done => use('readonly', (store, tx, keys = []) => {
+    //   const openCursor = (store.openKeyCursor || store.openCursor)
+    //   const req = openCursor.call(store)
+    //   tx.oncomplete = e => done(req.error, req.error ? undefined : keys)
+    //   req.onsuccess = () => {
+    //     const x = req.result
+    //     if (x) (keys.push(x.key), x.continue())
+    //   }
+    // })
     // key: (n, done) => (n < 0) ? done(null) : use('readonly', store => {
     //   var advanced = false
     //   var req = store.openCursor()
@@ -79,8 +99,7 @@ function kvidb (opts) {
     // And openKeyCursor isn't supported by Safari.
     // tx.oncomplete = () => done(null, keys)
   }
-  return api
+  */
 }
-
 },{}]},{},[1])(1)
 });
